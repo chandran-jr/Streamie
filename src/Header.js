1935b045c23660b3e79846bb694c3df8505497ed
Govind B Chandran <govindchandran235@gmail.com>
2021-09-23 00:51:46 +0530

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
     return (
         <div className="header">
+
             <MenuIcon className="burger"/>
-             <img className="yt-logo" src="https://www.depts.ttu.edu/provost/humanities-center/images/YouTubeLogo.jpg" alt="YouTube Logo" />
-            <input type="text"/>
+
+             <img className="yt_logo" src="https://www.depts.ttu.edu/provost/humanities-center/images/YouTubeLogo.jpg" alt="YouTube Logo" />
+
+             <div className="searchBar__div">
+             <input placeholder="Search" className="searchBar" type="text"/>
+             <div className="searchIcon__div">
+             <SearchIcon className="searchIcon"/>
+             </div>
+             </div>
         </div>
     )
 }
