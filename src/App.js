e05129ca934857c7f27a3a74b7009a65de117c92
Govind B Chandran <govindchandran235@gmail.com>
2021-09-22 23:00:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Sidebar from './Sidebar';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-     
+
+          <Header/>
+          
+          <Sidebar/>
+
     </div>
   );
 }
