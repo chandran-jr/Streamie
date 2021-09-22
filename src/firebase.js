87832b04090fc30999b775a598f376714a80f7b1
Govind B Chandran <govindchandran235@gmail.com>
2021-09-22 22:12:51 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANk93RQ_oYQohvku7AKstclwNx3ewu68I",
+    authDomain: "streamie-ba5f8.firebaseapp.com",
+    projectId: "streamie-ba5f8",
+    storageBucket: "streamie-ba5f8.appspot.com",
+    messagingSenderId: "1036132184731",
+    appId: "1:1036132184731:web:175492258cb3955df48c3e"
+  };
