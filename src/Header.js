c738a39b5ba1674c8e17468a65af60214eafbd89
Govind B Chandran <govindchandran235@gmail.com>
2021-09-23 01:39:42 +0530

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Avatar from '@mui/material/Avatar';
 
 function Header() {
     return (
@@ -16,6 +20,13 @@ function Header() {
              <div className="searchIcon__div">
              <SearchIcon className="searchIcon"/>
              </div>
+             </div>
+
+             <div className="header__icons">
+                 <VideoCallIcon/>
+                 <AppsIcon/>
+                 <NotificationsIcon/>
+                 <Avatar alt="Remy Sharp" src="https://i.pinimg.com/originals/11/05/0c/11050c5daab38e515f788abbf474e795.jpg" />
              </div>
         </div>
     )
