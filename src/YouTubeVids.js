0f175bb9e57ca27f85748489840e763c9f1b4fe9
Govind B Chandran <govindchandran235@gmail.com>
2021-09-24 10:32:14 +0530

    Add files via upload

--- src/YouTubeVids.js
+++ src/YouTubeVids.js
@@ -5,10 +5,16 @@ import Video from './Video';
 function YouTubeVids() {
     return (
         <div className="youtubevids">
+
+            <div className="youtubevids__vids">
              <Video image="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" title="Have a look at my Cat" channel="Cat" views="9M" avatarimg="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" />
              <Video image="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" title="Have a look at my Cat" channel="Cat" views="9M" avatarimg="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" />
              <Video image="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" title="Have a look at my Cat" channel="Cat" views="9M" avatarimg="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" />
              <Video image="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" title="Have a look at my Cat" channel="Cat" views="9M" avatarimg="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" />
+             <Video image="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" title="Have a look at my Cat" channel="Cat" views="9M" avatarimg="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" />
+             
+            </div>
+             
         </div>
     )
 }
