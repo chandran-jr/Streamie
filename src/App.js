d6308a9aa1cdd5bf9ce280b665488effad31785f
Govind B Chandran <govindchandran235@gmail.com>
2021-09-25 18:41:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import './App.css';
 import Sidebar from './Sidebar';
 import Header from './Header';
 import Videos from './Videos';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="app">
 
-          <Header/>
+        <Router>
+          <Switch>
 
-          <div className="app__Container">
-          <Sidebar/>
-          <Videos/>
-          </div>
+          <Route path="/search">
+              <Header/>
+            </Route>
 
+            <Route path="/">
+            <Header/>
+            <div className="app__Container">
+              <Sidebar/>
+              <Videos/>
+            </div>
+            </Route>
+            
+          </Switch>
+        </Router>
 
     </div>
   );
