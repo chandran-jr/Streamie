4c709e67c7e90689a85cfbcd8053e2e9d6588491
Govind B Chandran <govindchandran235@gmail.com>
2021-09-25 19:55:09 +0530

    Add files via upload

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './SearchPage.css'
+import './SearchPage.css';
+import TuneIcon from '@mui/icons-material/Tune';
 
 function SearchPage() {
     return (
         <div className="searchpage">
-            
+
+            <div className="searchpage__filter">
+                <TuneIcon className="searchpage__filtericon"/>
+                <h4>FILTERS</h4>
+            </div>
+
+            <hr className="searchpage__hr"/>
+
         </div>
     )
 }
